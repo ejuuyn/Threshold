24cfb5525a11c5ca5eeb6c94c621b4c3e96ad06c
idochoi <103577868+idochoi@users.noreply.github.com>
2022-04-13 22:00:11 +0900

    moya

---
+++ iamido/kiki.R
@@ -0,0 +1,13 @@
+print("hello world!")
+a=1
+# b=2 ----
+
+installed.packages("dplyr")
+library(dplyr)
+installed.packages("lubridate")
+library(lubrydate)
+install.packages("data.table")
+install.packages("srtingr")
+library(date.table)
+library()
+.libpaths()
